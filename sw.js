c03096f12f22f6d0d67f5b9d33c0f624724031d7
vctrsoares <victorsoares1952@gmail.com>
2026-05-30 14:34:17 -0300

    Bump SW cache to v3 to force refresh of 308-question bank

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fisio-quiz-v2';
+const CACHE = 'fisio-quiz-v3';
 const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
